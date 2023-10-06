856f0248dfdeb9fce56b970c09734ec7bfdfd477
Bruno Prestes <brunoasprestes@gmail.com>
2023-10-06 13:46:42 -0300

    ajuste titulo topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -10,7 +10,7 @@ const TopBar = () => {
   return (
     <div className="w-full bg-[#283618] lg:h-12 flex justify-between items-center">
         <div className="p-3 flex">
-            <h1 className="font-bold text-[#FEFAE0]">Instituto Amparo - </h1>
+            <h1 className="font-bold text-[#FEFAE0]">Instituto Amparo</h1>
             <h1 className="hidden lg:block text-[#FEFAE0]"> Tel: (96)99135-4590 - E-Mail: clinicadracamilaneiva@gmail.com</h1>
         </div>
 
